45d5836fc42834e5c47fde9b1887b892f3f635ee
Rick Cruz <rick.cruz@siteminder.com>
2022-07-26 14:36:51 +0800

    Update TransactionService fetch

--- src/main/kotlin/com/thirdparty/service/TransactionService.kt
+++ src/main/kotlin/com/thirdparty/service/TransactionService.kt
@@ -1,12 +1,13 @@
 package com.thirdparty.service
 
+import com.thirdparty.data.GetTransactionRequest
 import com.thirdparty.data.GetTransactionResponse
 import org.springframework.stereotype.Service
 
 @Service
 class TransactionService {
 
-    fun fetch(): GetTransactionResponse {
+    fun fetch(request: GetTransactionRequest): GetTransactionResponse {
         return GetTransactionResponse("ref23543543")
     }
 }
